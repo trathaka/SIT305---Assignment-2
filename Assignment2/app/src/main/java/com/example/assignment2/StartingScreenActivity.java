eb71fa62973a1cc22729eb971c90d3868eff3172
SUNNY RATHAKANANEK <trathaka@deakin.edu.au>
2020-06-09 18:12:58 +1000

    Added difficulty options

--- Assignment2/app/src/main/java/com/example/assignment2/StartingScreenActivity.java
+++ Assignment2/app/src/main/java/com/example/assignment2/StartingScreenActivity.java
@@ -7,21 +7,24 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class StartingScreenActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_QUIZ = 1;
+    public static final String EXTRA_DIFFICULTY = "extraDifficulty";
 
     public static final String SHARED_PREFS = "sharePrefs";
     public static final String KEY_HIGH_SCORE = "keyHighScore";
 
     private TextView textViewHighScore;
+    private Spinner spinnerDifficulty;
 
     private int highScore;
-
 
 
     @Override
@@ -30,6 +33,13 @@ public class StartingScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starting_screen);
 
         textViewHighScore = findViewById(R.id.text_view_highscore);
+        spinnerDifficulty = findViewById(R.id.spinner_difficulty);
+
+        String[] difficultyLevels = Question.getAllDifficultyLevels();
+        ArrayAdapter<String> adapterDifficulty = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, difficultyLevels);
+        adapterDifficulty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDifficulty.setAdapter(adapterDifficulty);
+
         Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
         buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +50,10 @@ public class StartingScreenActivity extends AppCompatActivity {
     }
 
     private void startQuiz() {
+        String difficulty = spinnerDifficulty.getSelectedItem().toString();
+
         Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        intent.putExtra(EXTRA_DIFFICULTY, difficulty);
         startActivityForResult(intent, REQUEST_CODE_QUIZ);
     }
 
