7f6444362f82955be8415d9d6856a61dadfc0590
SUNNY RATHAKANANEK <trathaka@deakin.edu.au>
2020-06-11 21:48:49 +1000

    Added login with Google Account intergration

--- Assignment2/app/src/main/java/com/example/assignment2/SplashScreen.java
+++ Assignment2/app/src/main/java/com/example/assignment2/SplashScreen.java
@@ -16,18 +16,18 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
                 .withFullScreen()
-                .withTargetActivity(LanguageSelection.class)
+                .withTargetActivity(LoginPage.class)
                 .withSplashTimeOut(2000)
                 .withBackgroundColor(Color.parseColor("#1a1b28"))
-                .withHeaderText("Header")
+                //.withHeaderText("Header")
                 .withFooterText("Footer")
-                .withBeforeLogoText("Before Logo Text")
+                //.withBeforeLogoText("Before Logo Text")
                 .withAfterLogoText("After Logo Text")
                 .withLogo(R.mipmap.ic_launcher_round);
 
-        config.getHeaderTextView().setTextColor(Color.WHITE);
+        //config.getHeaderTextView().setTextColor(Color.WHITE);
         config.getFooterTextView().setTextColor(Color.WHITE);
-        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        //config.getBeforeLogoTextView().setTextColor(Color.WHITE);
         config.getAfterLogoTextView().setTextColor(Color.WHITE);
 
         View splashScreen = config.create();
